5bf16e3d5eda24bf67b62478deda0df97e2c88b0
thebabalola <t.babalolajoseph@gmail.com>
2023-10-05 18:06:56 -0700

    e commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - serves as the starting and entry point
+ * Description: This C program prints the sizes of various data types.
+ * Return: 0 if properly exited (sucess).
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Size of a char: %zu byte(s)\n", sizeof(char));
+    printf("Size of an int: %zu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+    return 0;
+}
+
