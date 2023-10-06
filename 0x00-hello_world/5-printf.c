ea238e9e1ac85730044a09dbfd88205fd81ea34d
thebabalola <t.babalolajoseph@gmail.com>
2023-10-05 17:22:39 -0700

    d commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the world in 'printf'
+ * Description: This C program prints what is held in the printf fumction
+ * Return: 0 if properly exited (sucess).
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
