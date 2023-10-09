c74b9fd51348dc7de9203cec4b275bfae7976abd
thebabalola <t.babalolajoseph@gmail.com>
2023-10-09 23:09:19 +0100

    print 101 commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,28 +8,33 @@
 
 int main(void)
 {
-	int firstDigit, secondDigit, thirdDigit;
+	int digi01, digi02, digi03;
 
-	for (firstDigit = 0; firstDigit <= 7; firstDigit++)
+	for (digi01 = 0; digi01 <= 3; digi01++)
 	{
-		for (secondDigit = firstDigit + 1; secondDigit <= 8; secondDigit++)
+		for (digi02 = digi01 + 1; digi02 <= 5; digi02++)
 		{
-			for (thirdDigit = secondDigit + 1; thirdDigit <= 9; thirdDigit++)
+			for (digi03 = digi03 + 1; digi03 <= 7; digi02++)
 			{
-				putchar(firstDigit + '0');  // Print the first digit
-				putchar(secondDigit + '0'); // Print the second digit
-				putchar(thirdDigit + '0');  // Print the third digit
+			/* prints first digit - digi01 */
+			putchar(digi01 + '0');
+			/* prints second digit - digi02 */
+			putchar(digi02 + '0');
+			/* prints third digit - digi03 */
+			putchar(digi03 + '0');
 
-				if (firstDigit != 7 || secondDigit != 8 || thirdDigit != 9)
-				{
-					putchar(','); // Print a comma
-					putchar(' '); // Print a space
-				}
+			if (digi01 != 3 || digi02 != 5 || digi03 != 7)
+			{
+				/* prints comma */
+				putchar(',');
+				/* prints a sapce */
+				putchar(' ');
+			}
 			}
 		}
 	}
 
-	putchar('\n'); // Print a newline character
+	putchar('\n'); /* prints newline character */
 
-	return 0;
+	return (0);
 }
