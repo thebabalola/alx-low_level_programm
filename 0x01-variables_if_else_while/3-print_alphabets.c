896654b8ddeab92dd9d3e4abd9d965d812815d55
thebabalola <t.babalolajoseph@gmail.com>
2023-10-09 19:41:46 +0100

    print alpha commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lowercase
+ * followed by a new line
+ * Return: should always return 0 (Success)
+*/
+int main(void)
+{
+	char alphabet;
+
+	/* Prints lowercase alphabets */
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	/* Prints uppercase alphabet */
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');  /* Prints a newline character */
+
+	return (0);
+}
