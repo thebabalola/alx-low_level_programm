f668174f45dc17214116faac66d2f5b277a8ba63
thebabalola <t.babalolajoseph@gmail.com>
2023-10-09 22:58:57 +0100

    print 100 comb commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combination of two digits
+ * followed by a new line
+ * Return: should always return 0 (Success)
+ */
+
+int main(void)
+{
+	int digi01, digi02;
+
+	for (digi01 = 0; digi01 <= 5; digi01++)
+	{
+		for (digi02 = digi01 + 1; digi02 <= 7; digi02++)
+		{
+			/* prints first digit - digi01 */
+			putchar(digi01 + '0');
+			/* prints second digit - digi02 */
+			putchar(digi02 + '0');
+
+			if (digi01 != 5 || digi02 != 7)
+			{
+				/* prints comma */
+				putchar(',');
+				/* prints a sapce */
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n'); /* prints newline character */
+
+	return (0);
+}
