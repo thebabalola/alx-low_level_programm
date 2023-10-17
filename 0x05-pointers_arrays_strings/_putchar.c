c058861d0de7d3136ea8a3626b00e57a13b5280b
thebabalola <t.babalolajoseph@gmail.com>
2023-10-18 00:32:32 +0100

    a newer commit

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to the standard output
+ *
+ * @c: The character to print
+
+ * * Return: On success, 1 is returned. On error, -1 is returned.
+ */
+
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
