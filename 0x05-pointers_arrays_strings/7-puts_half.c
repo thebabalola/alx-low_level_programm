10316cc4b5915bf4b8504f8241297e5f359c276c
thebabalola <t.babalolajoseph@gmail.com>
2023-10-18 01:31:12 +0100

    added commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ *
+ * @str: a character array string
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	i = (length - 1) / 2;
+	i++;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void _puts(char *str);
 void print_rev(char *s);
 int _putchar(char c);
 void rev_string(char *s);
-
+void puts2(char *str);
+void puts_half(char *str);
 
 #endif
