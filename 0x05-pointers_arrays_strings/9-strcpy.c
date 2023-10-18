8cc459221d7164f1931f60e959f4d801752ec41b
thebabalola <t.babalolajoseph@gmail.com>
2023-10-18 01:55:42 +0100

    recommit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,11 @@ char *_strcpy(char *dest, char *src)
 {
 	char *original_dest = dest;
 
-	while ((*dest = *src) != '\0') {
+	while ((*dest = *src) != '\0')
+	{
 		dest++;
 		src++;
 	}
 
-	return original_dest;
+	return (original_dest);
 }
