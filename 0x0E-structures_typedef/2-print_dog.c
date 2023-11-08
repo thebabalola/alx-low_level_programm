9579e971d993ad2051ff9ed80246b9d00b8de02a
thebabalola <t.babalolajoseph@gmail.com>
2023-11-08 01:18:49 +0100

    updated structures and typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 
-		printf("Age: %.1f\n", d->age);
+		printf("Age: %f\n", d->age);
 
 		if (!d->owner)
 			printf("Owner: (unavailable)\n");
