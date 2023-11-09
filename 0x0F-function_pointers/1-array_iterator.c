af383248e657f90e24812d5e6395869fadefab0c
thebabalola <t.babalolajoseph@gmail.com>
2023-11-09 15:02:47 +0100

    function pointer update commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,15 +6,18 @@
  * @array: the array to print.
  * @size: prints the size of the array.
  * @action: function to apply to each array element.
- * Return: no return.
+ * Return: no return value.
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (array != NULL && action != NULL)
+	{
 	for (i = 0; i < size; i++)
 	{
 		(*action)(array[i]);
+	}
 	}
 }
