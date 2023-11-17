84463484307cef3e9829ce2b64d103c27184695b
thebabalola <t.babalolajoseph@gmail.com>
2023-11-17 03:20:17 +0100

    update singly link

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - Function to be executed before main.
+ * Return: No return value.
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
